b0e815de2043ce54685b842c4460601ea415e160
nebulaM <nebulaM12@gmail.com>
2016-12-23 16:52:00 -0800

    add animation for record

--- app/src/main/java/com/github/android/bestpath/dialog/RecordDialog.java
+++ app/src/main/java/com/github/android/bestpath/dialog/RecordDialog.java
@@ -5,16 +5,22 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 import com.github.android.bestpath.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class RecordDialog extends DialogFragment {
     public static final String TAG="RecordDialog";
@@ -57,13 +63,34 @@ public class RecordDialog extends DialogFragment {
             public void onClick(DialogInterface dialog, int id) {
                 //TODO:Pop up another detailed dialog
             }});
+        //http://stackoverflow.com/questions/4852281/android-how-can-i-make-a-button-flash/4852468#4852468
+        final Animation animation = new AlphaAnimation(1.0f, 0.6f); // Change alpha from fully visible to invisible
+        animation.setDuration(2000);
+        animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+        animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
+        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+
 
         mRecordImage =(ImageView)view.findViewById(R.id.dialog_effect_image);
-        setRecordImage();
+        if(setRecordAnimation()) {
+            AnimationDrawable frameAnimation = (AnimationDrawable) mRecordImage.getDrawable();
+            frameAnimation.start();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run(){
+                    mRecordImage.startAnimation(animation);
+                }
+
+            },frameAnimation.getNumberOfFrames()*frameAnimation.getDuration(0));
+        }else {
+            mRecordImage.startAnimation(animation);
+        }
         mRecordImage.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO:Pop up another detailed dialog
+                mRecordImage.clearAnimation();
+
             }
         }));
 
@@ -81,7 +108,7 @@ public class RecordDialog extends DialogFragment {
         mOnCloseListener.onDialogClose(TAG,-1);
     }
 
-    private void setRecordImage(){
+    private boolean setRecordAnimation(){
         int count=0;
         for(int i=mRecordList.size()/2;i<mRecordList.size();++i){
             if(mRecordList.get(i)>=3){
@@ -92,26 +119,27 @@ public class RecordDialog extends DialogFragment {
         }
         switch (count){
             case 1:
-                mRecordImage.setImageResource(R.mipmap.record_1);
+                mRecordImage.setImageResource(R.drawable.record_animation_1);
                 break;
             case 2:
-                mRecordImage.setImageResource(R.mipmap.record_2);
+                mRecordImage.setImageResource(R.drawable.record_animation_2);
                 break;
             case 3:
-                mRecordImage.setImageResource(R.mipmap.record_3);
+                mRecordImage.setImageResource(R.drawable.record_animation_3);
                 break;
             case 4:
-                mRecordImage.setImageResource(R.mipmap.record_4);
+                mRecordImage.setImageResource(R.drawable.record_animation_4);
                 break;
             case 5:
-                mRecordImage.setImageResource(R.mipmap.record_5);
+                mRecordImage.setImageResource(R.drawable.record_animation_5);
                 break;
             case 6:
-                mRecordImage.setImageResource(R.mipmap.record_6);
+                mRecordImage.setImageResource(R.drawable.record_animation_6);
                 break;
             default:
-                mRecordImage.setImageResource(R.mipmap.record_0);
-                break;
+                mRecordImage.setImageResource(R.drawable.record_0);
+                return false;
         }
+        return true;
     }
 }
